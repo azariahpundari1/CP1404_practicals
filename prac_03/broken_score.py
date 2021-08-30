a23b24f80db33562716d7d87b08d5cceeefe2bea
azariahpundari1 <azariah.pundari@my.jcu.edu.au>
2021-08-30 19:08:57 +1000

    add random score results

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -2,13 +2,17 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
-
+import random
 
 # TODO: Fix this!
+
+
 def main():
     score = float(input("Enter score: "))
+    random_score = random.randint(0, 100)
+    random_score_result = get_result(random_score)
     result = get_result(score)
-    print(result)
+    print(f"A random score of {random_score:.1f} is {random_score_result}\nA score of {score} is {result}")
 
 
 def get_result(score):
