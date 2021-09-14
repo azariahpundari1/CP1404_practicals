d29788e6595c0c53bb5e7f49a0b0f02eadfc2837
azariahpundari1 <azariah.pundari@my.jcu.edu.au>
2021-09-14 22:49:58 +1000

    add word occurences program

--- prac_05/word_occurences.py
+++ prac_05/word_occurences.py
@@ -5,7 +5,7 @@ for word in number_of_words:
     count = words.get(word, 0)
     words[word] = count + 1
 words_to_count = list(words.keys())
-# words_to_count.sort()
+words_to_count.sort()
 
 for word in words:
     print(f'{word}: {words[word]}')
