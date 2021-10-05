3da6ca1dae3f34c9b6e66cdd74cd478ffe9eddb6
azariahpundari1 <azariah.pundari@my.jcu.edu.au>
2021-10-05 18:17:57 +1100

    add working program that converts miles -> km

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -1,4 +1,32 @@
 from kivy.app import App
 from kivy.lang import Builder
 
-MILES_TO_KM = 1.61
+
+MILES_TO_KM = 1.60934
+
+
+class ConvertMilesToKM(App):
+    def build(self):
+        self.title = "Convert Miles to Kilometres"
+        self.root = Builder.load_file("convert_miles_km.kv")
+        return self.root
+
+    def convert_miles(self):
+        value = self.valid_number()
+        kilometers = value * MILES_TO_KM
+        self.root.ids.output.text = str(kilometers)
+
+    def handle_increment(self, increment):
+        value = self.valid_number() + increment
+        self.root.ids.input.text = str(value)
+        self.convert_miles()
+
+    def valid_number(self):
+        value = float(self.root.ids.input.text)
+        if value >= 0:
+            return value
+        elif ValueError:
+            return 0
+
+
+ConvertMilesToKM().run()
